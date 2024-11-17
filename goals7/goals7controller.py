8420ec48c004b8cc2889dc15545ccff1b2742096
Ritali Jain <31087512+RitaliJ@users.noreply.github.com>
2024-11-17 04:31:57 -0800

    Multithreading camera and motor code to move the motors to the blue object
    Uses threads to share data between camera and motors
    Implements angle pan and angle tilt formulas (scaling, fov, pixel to angles, etc)
    Handles case where object is detected and creates a spline to it on the fly
    Includes multi object detection handling

---
+++ goals7/goals7controller.py
@@ -0,0 +1,21 @@
+'''goals7controller.py
+
+   This encapsulates the motor control code into a single function.
+'''
+
+#
+#   Define your entire code as a function.  The argument will be the
+#   shared data that is accessed by both pieces.
+#
+def controller(shared):
+    # Place all your code here.  Note you will have to indent EVERYTHING.
+    ...
+
+
+#
+#   Test
+#
+#   If you run "python3 goals7controller.py" this will be executed.
+#
+if __name__ == "__main__":
+    controller(None)
---
+++ goals7/goals7detector.py
@@ -0,0 +1,21 @@
+'''goals7detector.py
+
+   This encapsulates the object detector code into a single function.
+'''
+
+#
+#   Define your entire code as a function.  The argument will be the
+#   shared data that is accessed by both pieces.
+#
+def detector(shared):
+    # Place all your code here.  Note you will have to indent EVERYTHING.
+    ...
+
+
+#
+#   Test
+#
+#   If you run "python3 goals7detector.py" this will be executed.
+#
+if __name__ == "__main__":
+    detector(None)
